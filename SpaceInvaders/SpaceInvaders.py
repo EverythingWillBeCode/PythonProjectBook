b6f89e20afdd2aa7ed700caa7a6153fef9b719d3
Christian! <67934837+EverythingWillBeCode@users.noreply.github.com>
2023-05-18 19:47:50 -0300

    update

--- SpaceInvaders/SpaceInvaders.py
+++ SpaceInvaders/SpaceInvaders.py
@@ -60,18 +60,98 @@ class Alien(pygame.sprite.Sprite):
             
 class Ammo(pygame.sprite.Sprite):
     def __init__(self, color, (width, height)):
-pygame.sprite.Sprite.__init__(self)
-self.image = pygame.Surface([width, height])
-self.image.fill(color)
-self.rect = self.image.get_rect()
-self.speed = 0
-self.vector = 0
-def update(self):
-self.rect.y += self.vector * self.speed
-if self.rect.y < 0 or self.rect.y > RES[1]:
-self.kill()
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.speed = 0
+        self.vector = 0
+    def update(self):
+        self.rect.y += self.vector * self.speed
+        if self.rect.y < 0 or self.rect.y > RES[1]:
+            self.kill()
 class Block(pygame.sprite.Sprite):
-def __init__(self, color, (width, height)):
-pygame.sprite.Sprite.__init__(self)
-self.image = pygame.Surface([width, height])
-self.rect = self.image.get_rect()
+    def __init__(self, color, (width, height)):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        
+class GameState:
+    pass
+
+class Game(object):
+    def __init__(self):
+        pygame.init()
+        pygame.font.init()
+        self.clock = pygame.time.Clock()
+        self.game_font = pygame.font.Font(
+        ‘data/Orbitracer.ttf’, 28)
+        self.intro_font = pygame.font.Font(
+        ‘data/Orbitracer.ttf’, 72)
+        self.screen = pygame.display.set_mode([RES[0], RES[1]])
+        self.time = pygame.time.get_ticks()
+        self.refresh_rate = 20
+        self.rounds_won = 0
+        self.level_up = 50
+        self.score = 0
+        self.lives = 2
+        self.player_group = pygame.sprite.Group()
+        self.alien_group = pygame.sprite.Group()
+        self.bullet_group = pygame.sprite.Group()
+        self.missile_group = pygame.sprite.Group()
+        self.barrier_group = pygame.sprite.Group()
+        self.all_sprite_list = pygame.sprite.Group()
+        self.intro_screen = pygame.image.load(‘data/start_screen.jpg’).convert()
+        self.background = pygame.image.load(‘data/Space-Background.jpg’).convert()
+        pygame.display.set_caption(‘Pivaders - ESC to exit’)
+        pygame.mouse.set_visible(False)
+        Player.image = pygame.image.load(
+        ‘data/ship.png’).convert()
+        Player.image.set_colorkey(BLACK)
+        Alien.image = pygame.image.load(
+        ‘data/Spaceship16.png’).convert()
+        Alien.image.set_colorkey(WHITE)
+        GameState.end_game = False
+        GameState.start_screen = True
+        GameState.vector = 0
+        GameState.shoot_bullet = False
+        
+def control(self):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            GameState.start_screen = False
+            GameState.end_game = True
+        if event.type == pygame.KEYDOWN \and event.key == pygame.K_ESCAPE:
+            if GameState.start_screen:
+                GameState.start_screen = False
+                GameState.end_game = True
+                self.kill_all()
+            else:
+                GameState.start_screen = True
+    self.keys = pygame.key.get_pressed()
+    if self.keys[pygame.K_LEFT]:
+        GameState.vector = -1
+    elif self.keys[pygame.K_RIGHT]:
+        GameState.vector = 1
+    else:
+        GameState.vector = 0
+    if self.keys[pygame.K_SPACE]:
+        if GameState.start_screen:
+            GameState.start_screen = False
+            self.lives = 2
+            self.score = 0
+            self.make_player()
+            self.make_defenses()
+            self.alien_wave(0)
+        else:
+            GameState.shoot_bullet = True
+            
+def splash_screen(self):
+    while GameState.start_screen:
+        self.kill_all()
+        self.screen.blit(self.intro_screen, [0, 0])
+        self.screen.blit(self.intro_font.render(
+        “PIVADERS”, 1, WHITE), (265, 120))
+        self.screen.blit(self.game_font.render(
+        “PRESS SPACE TO PLAY”, 1, WHITE), (274, 191))
